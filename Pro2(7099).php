fa76e8dc0222e69691ae79535de07c1ff1ca203d
YashParmar88 <parmaryash1405@gmail.com>
2026-06-12 23:44:13 +0530

    Add files via upload

---
+++ Pro2(7099).php
@@ -0,0 +1,14 @@
+<?php
+$a = 77;
+$b = 88;
+$c = 99;
+
+echo "<h3>Maximum and Minimum Number</h3>";
+
+$max = max($a, $b, $c);
+$min = min($a, $b, $c);
+
+echo "Numbers: $a, $b, $c <br/>";
+echo "Maximum Number: $max <br/>";
+echo "Minimum Number: $min <br/>";
+?>
