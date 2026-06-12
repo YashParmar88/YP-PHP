d63e5a3872e9eb6a10ddf9293a6715b2b89b604b
YashParmar88 <parmaryash1405@gmail.com>
2026-06-12 23:41:56 +0530

    Add files via upload

---
+++ Pro1(7099).php
@@ -0,0 +1,13 @@
+<?php
+$str="Marwadi university";
+$name = "YP";
+$semester = "Semester 5";
+$spi = 5.25;
+$cpi = 6.50;
+echo "<h2>Previous Semester Result</h2>";
+echo"College:$str <br/>";
+echo "Student Name: " . $name . "<br>";
+echo "Semester: " . $semester . "<br>";
+echo "SPI: " . $spi . "<br>";
+echo "CPI: " . $cpi . "<br>";
+?>
