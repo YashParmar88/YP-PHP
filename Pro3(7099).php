b1c7d390a253b0d9f72ca9b1929de20da7517037
YashParmar88 <parmaryash1405@gmail.com>
2026-06-18 21:33:03 +0530

    Add files via upload

---
+++ Pro3(7099).php
@@ -0,0 +1,19 @@
+<?php
+
+$a = 50;
+$b = 30;
+
+echo "<h2>Operators in PHP</h2>";
+
+echo "Value of A = " . $a . "<br>";
+echo "Value of B = " . $b . "<br><br>";
+
+echo "<h3>Arithmetic Operators</h3>";
+
+echo "Addition = " . ($a + $b) . "<br>";
+echo "Subtraction = " . ($a - $b) . "<br>";
+echo "Multiplication = " . ($a * $b) . "<br>";
+echo "Division = " . ($a / $b) . "<br>";
+echo "Percentage = " . ($a % $b) . "<br>";
+
+?>
